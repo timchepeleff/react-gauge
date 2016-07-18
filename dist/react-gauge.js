993bc76a169bbb0bd7038f629d4d323640af44c3
Mike Varano <mikevee313@gmail.com>
2016-07-18 14:52:11 -0400

    bundled

--- dist/react-gauge.js
+++ dist/react-gauge.js
@@ -1,10 +1,10 @@
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
@@ -37,7 +37,7 @@ var defaultProps = {
   value: 0
 };
 
-var ReactGauge = function (_React$Component) {
+var ReactGauge = (function (_React$Component) {
   _inherits(ReactGauge, _React$Component);
 
   function ReactGauge(props) {
@@ -180,7 +180,7 @@ var ReactGauge = function (_React$Component) {
   }]);
 
   return ReactGauge;
-}(_react2.default.Component);
+})(_react2.default.Component);
 
 exports.default = ReactGauge;
 
